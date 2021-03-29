6f49cbdb2ebe4bab8fb6deab902185025335fe0b
Jeongmin Yeo <yjh930919>
2021-03-29 21:02:01 +0900

    edit workflow

---
+++ containers/request-service/src/main/java/com/example/springcicddemo/App.java
@@ -0,0 +1,48 @@
+package com.example.springcicddemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+	@Autowired
+	RestTemplateBuilder restTemplateBuilder;
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(){
+		try {
+			InetAddress localHost = InetAddress.getLocalHost();
+			return String.format("host-address: %s host-name: %s",
+					localHost.getHostAddress(),
+					localHost.getHostName());
+		} catch (UnknownHostException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@GetMapping("/request")
+	public String requestToOtherServer(){
+		RestTemplate restTemplate = restTemplateBuilder.build();
+
+		String serviceName = "response";
+		String servicePort = "8080";
+		String serviceUri = "/api/hello";
+
+		String response = restTemplate.getForObject(String.format("http://%s:%s/%s", serviceName, servicePort, serviceUri), String.class);
+		return response;
+	}
+}
